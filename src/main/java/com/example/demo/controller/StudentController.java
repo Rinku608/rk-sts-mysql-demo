223365ea858b248c0bffb1ce22f4ab93a572bcf7
Rinku608 <rinku.kumari@mavenwave.com>
2022-12-06 16:19:58 +0530

    new commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -44,5 +45,12 @@ public class StudentController {
 	{  
 		return studentService.saveOrUpdate(student);  
 	  
+	}  
+	
+	@PutMapping("/students")  
+	private Student update(@RequestBody Student student)   
+	{  
+		studentService.saveOrUpdate(student);  
+	return student;  
 	}  
 }
